edbca95a1427ad1326e2f355752a51d6ba2fbd69
Shakil <shakil1emperor@gmail.com>
2023-12-16 18:17:59 +0600

    Add files via upload

---
+++ 2. if-else condition/if-else-if.js
@@ -0,0 +1,14 @@
+a = 20
+b = 50
+c = 60 
+
+if (a>b & a>c) {
+  console.log("a is greater than b & c")
+}
+
+else if (b>a & b>c){
+  console.log("b is greater than a & c")
+}
+else{
+  console.log('c is greater than a & b')
+}
