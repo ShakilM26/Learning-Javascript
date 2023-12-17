1e0ae505094f27b831c575817f9eff3632160c86
Shakil <shakil1emperor@gmail.com>
2023-12-17 16:25:07 +0600

    Add files via upload

---
+++ 4. loop/1. for loop basic.js
@@ -0,0 +1,23 @@
+// for (initialExpression; condition; updateExpression) {
+    // statements}
+    
+// first example
+
+var num = 0;
+for (i = num; i <= 5; i++){
+  console.log(i);
+}
+
+// another example
+
+var num = 5;
+for (i=1; i<=num; i++){
+  console.log('I love Javascript');
+}
+
+// without the initializer
+
+var i = 0;
+for (; i<10; i++) {
+  console.log(i);
+}
