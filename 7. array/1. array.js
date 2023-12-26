2334c36d4bc029e16c6fae46c2cd49ad5f982824
Shakil <shakil1emperor@gmail.com>
2023-12-27 00:34:54 +0600

    Add files via upload

--- 7. array/1. array.js
+++ 7. array/1. array.js
@@ -1,1 +1,21 @@
+// In JavaScript, an array is an ordered list of values. 
+// Each value is called an element specified by an index. 
 
+
+// There are two ways to create an array
+
+// 1. Array constructor
+items = new Array('Red', 'Blue', 'Black', 'Green', 'Yellow')
+console.log(items)
+
+// 2. Using []
+names = ['Peter', 'Tony', 'Steve', 'Stephen']
+console.log(names)
+
+// Accessing, Change the value  
+mountains = ['Everest', 'Fuji', 'Mount Logan']
+mountains[2] = 'Nanga Parbat';
+console.log(mountains)
+
+// length
+console.log(mountains.length)
